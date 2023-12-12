7e91d70fe6000cebfc8607cfaba6fd66a5ac5f0b
Darren Coutts <couttsdarren@outlook.com>
2023-12-12 13:20:49 +0000

    Start implementing base component

---
+++ src/Components/BaseComponent.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Appkit\UI\Components;
+
+use Illuminate\View\Component as BladeComponent;
+
+abstract class BaseComponent extends BladeComponent
+{
+    public function __construct(array $attributes = []) {
+        parent::__construct($attributes);
+    }
+}
