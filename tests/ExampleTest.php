9b59c5a949c91815f933a61d4e8036e910dbdd3a
Darren Coutts <couttsdarren@outlook.com>
2023-12-13 08:46:35 +0000

    Upgrade to pest

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,24 +1,7 @@
 <?php
 
-namespace AppKit\Ui\Tests;
+uses(\AppKit\Ui\Tests\TestCase::class);
 
-use AppKit\Ui\Tests\Models\Article;
-
-class ExampleTest extends TestCase
-{
-    /** @test */
-    public function trueIsTrue()
-    {
-        $this->assertTrue(true);
-    }
-
-    /** @test */
-    public function articlesCanBeLoaded()
-    {
-        // create 5 articles
-        factory(Article::class, 5)->create();
-
-        // check the database for 5 articles
-        $this->assertEquals(5, Article::count());
-    }
-}
+test('true is true', function () {
+    expect(true)->toBeTrue();
+});
