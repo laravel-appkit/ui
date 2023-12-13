4e660c451d0c647aa2496da8698f08f8092d8387
Darren Coutts <couttsdarren@outlook.com>
2023-12-13 13:11:00 +0000

    Improve the tests

--- tests/AttributeBagTest.php
+++ tests/AttributeBagTest.php
@@ -5,20 +5,21 @@ use Illuminate\View\ComponentAttributeBag;
 
 uses(\AppKit\UI\Tests\TestCase::class);
 
-beforeEach(function () {
-    $attributeBag = new ComponentAttributeBag();
+function createAttributeBuilder($attributes = [], $options = [])
+{
+    $attributeBag = new ComponentAttributeBag($attributes);
 
-    $this->attributeBuilder = new AttributeBuilder($attributeBag, collect());
-});
+    return new AttributeBuilder($attributeBag, collect($options));
+}
 
 it('can be initialised with an empty attribute bag', function () {
-    expect($this->attributeBuilder->getAttributeBag()->jsonSerialize())->toBeEmpty();
+    expect(createAttributeBuilder()->getAttributes())->toBeEmpty();
 });
 
 it('can add classes in multiple ways', function (string $method, $data) {
-    $attributeBuilder = $this->attributeBuilder->addClass($data);
+    $attributeBuilder = createAttributeBuilder()->addClass($data);
 
-    expect($attributeBuilder->jsonSerialize())->toHaveKey('class', 'class-1 class-2 class-3 class-4 class-5');
+    expect($attributeBuilder->getAttributes())->toHaveKey('class', 'class-1 class-2 class-3 class-4 class-5');
 })->with([
     ['as a single string', 'class-1 class-2 class-3 class-4 class-5'],
     ['as an array', ['class-1', 'class-2', 'class-3', 'class-4', 'class-5']],
