5f60fcd19c35bd7779fe6488b52c6ed1b5a411f7
Darren Coutts <couttsdarren@outlook.com>
2023-12-14 13:07:25 +0000

    Setup the addition of attributes

--- src/AttributeBuilder.php
+++ src/AttributeBuilder.php
@@ -5,14 +5,24 @@ namespace AppKit\UI;
 use BadMethodCallException;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
 use Illuminate\Support\Traits\ForwardsCalls;
 use Illuminate\View\ComponentAttributeBag;
 use InvalidArgumentException;
+use ReflectionMethod;
 use Symfony\Component\HttpFoundation\Session\Attribute\AttributeBag;
 
 class AttributeBuilder
 {
     use ForwardsCalls;
+
+    protected $magicMethodRegex = <<<'EOD'
+    /
+    (?P<operation>set|add|remove|toggle)
+    (?P<attribute>[A-Za-z0-9]*)
+    (?P<type>Attribute|Class)
+    /x
+    EOD;
 
     public function __construct(
         protected ComponentAttributeBag &$attributeBag,
@@ -111,14 +121,28 @@ class AttributeBuilder
         return $this;
     }
 
+    public function removeAttribute(...$attributes): AttributeBuilder
+    {
+        // flatten the arguments to the function
+        $attributes = Arr::flatten($attributes);
+
+        // remove each of the attributes from the attribute bag
+        foreach ($attributes as $attribute) {
+            $this->offsetUnset($attribute);
+        }
+
+        // return a fluent API
+        return $this;
+    }
+
     /**
      * Get an attribute from the attribute bag
      *
      * @param mixed $attribute
      * @param mixed $default
-     * @return AttributeBag
+     * @return mixed
      */
-    public function getAttribute($attribute, $default = null): AttributeBag
+    public function getAttribute($attribute, $default = null): mixed
     {
         // get the attribute from the attribute bag
         return $this->get($attribute, $default);
@@ -160,6 +184,75 @@ class AttributeBuilder
      */
     public function __call($method, $parameters)
     {
+        // an array to store any possible matches from the magic method regex
+        $magicMethodRegexMatches = [];
+
+        if (preg_match($this->magicMethodRegex, $method, $magicMethodRegexMatches)) {
+            $magicMethodParameterNames = ['attribute', 'value'];
+
+            // we alias the add operation to set
+            if ($magicMethodRegexMatches['operation'] == 'add') {
+                $magicMethodRegexMatches['operation'] = 'set';
+            }
+
+            // calculate the name of the method that we actually want to call
+            $methodName = $magicMethodRegexMatches['operation'] . $magicMethodRegexMatches['type'];
+
+            // the methods that we are allowed to call via the magic method
+            $allowedMethods = [
+                'addAttribute',
+                'setAttribute',
+                'removeAttribute'
+            ];
+
+            // check that it is an allowed method
+            if (in_array($methodName, $allowedMethods)) {
+                // get the reflection of the method that we are ultimately going to call
+                $methodReflection = new ReflectionMethod($this, $methodName);
+
+                // get the parameters of that method
+                $methodParametersReflection = $methodReflection->getParameters();
+
+                // create an array to store the parameters that we will actually pass to the method
+                $methodParameterValues = [];
+
+                foreach ($methodParametersReflection as $callableParameter) {
+                    // get the name of the parameter
+                    $parameterName = $callableParameter->getName();
+
+                    // check if it is variadic
+                    if ($callableParameter->isVariadic()) {
+                        // if it is, then the name is going to be the singular name of the method
+                        $parameterName = Str::of($parameterName)->singular()->__toString();
+                    }
+
+                    // check if we have a matching parameter in the regex matches
+                    if (array_key_exists($parameterName, $magicMethodRegexMatches)) {
+                        // if we do, we get that value and set it on the array that will be passed to the method
+                        $methodParameterValues[$parameterName] = lcfirst($magicMethodRegexMatches[$parameterName]);
+
+                        // we then remove the parameter name from the list that we are still looking for
+                        $magicMethodParameterNames = array_diff($magicMethodParameterNames, [$parameterName]);
+                    }
+                }
+
+                // because we have removed some things from the array, we want to reset the keys
+                $magicMethodParameterNames = array_values($magicMethodParameterNames);
+
+                // loop through everything that is left
+                foreach ($magicMethodParameterNames as $parameterPosition => $parameterName) {
+                    // and check if it was passed through as a parameter to the magic method
+                    if (isset($parameters[$parameterPosition])) {
+                        // if it was, then we add it to the list of parameters
+                        $methodParameterValues[$parameterName] = $parameters[$parameterPosition];
+                    }
+                }
+
+                // finally, we call the underlying method
+                return call_user_func_array([$this, $methodName], $methodParameterValues);
+            }
+        }
+
         return $this->forwardCallTo($this->attributeBag, $method, $parameters);
     }
 }
