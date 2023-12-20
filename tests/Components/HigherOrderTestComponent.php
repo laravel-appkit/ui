4dfcda861509f10792fe8d5717e3badb46ac0a8e
Darren Coutts <couttsdarren@outlook.com>
2023-12-20 09:13:36 +0000

    document

--- tests/Components/HigherOrderTestComponent.php
+++ tests/Components/HigherOrderTestComponent.php
@@ -12,9 +12,12 @@ class HigherOrderTestComponent extends TestComponent
 
     public function __construct()
     {
+        // store the last instance
         static::$lastInstance = $this;
 
+        // check if we are extending a class that has a constructor
         if (method_exists(parent::class, '__construct')) {
+            // call the parent constructor with all of the arguments passed
             parent::__construct(...func_get_args());
         }
     }
