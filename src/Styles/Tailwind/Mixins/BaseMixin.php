948811bd10ef658abc735f6f19c256435c8527a4
darrencoutts118 <darrencoutts118@users.noreply.github.com>
2025-10-24 11:09:20 +0000

    PSR-12 Coding Standards

--- src/Styles/Tailwind/Mixins/BaseMixin.php
+++ src/Styles/Tailwind/Mixins/BaseMixin.php
@@ -28,7 +28,7 @@ abstract class BaseMixin
      *
      * @return void
      */
-    public final function __construct(...$options)
+    final public function __construct(...$options)
     {
         $this->tailwind = new Tailwind();
 
